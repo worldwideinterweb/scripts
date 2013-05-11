80007af70df1ecda66e3832a5b2dd3d8c0e667c2
root <root@wwi-nginx01.worldwideinterweb.com>
2013-05-11 12:42:26 +1000

    added initial files

---
+++ webhosts/cron/sync_web.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# set paths / dirs
+_paths="/opt/joomla-173/"
+#/var/lib/php/session/
+#/home/vivek/ \
+#/projects/scripts/*.pl
+ 
+# binary file name
+_unison=/usr/bin/unison
+ 
+# server names 
+# sync server1.cyberciti.com with rest of the server in cluster
+_rserver="10.1.1.30 10.1.1.37 10.1.1.39"
+ 
+# sync it
+for r in ${_rserver}
+do
+	for p in ${_paths}
+	do
+        	${_unison} Sync -batch "${p}"  "ssh://${r}/${p}"
+	done
+done
